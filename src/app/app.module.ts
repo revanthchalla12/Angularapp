73e48e9746e30f85b913b77ec8b84fa18db6ea14
Revanth reddy <revanthchalla944@gmail.com>
2023-06-14 22:37:31 -0500

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      NavigationComponent,
+      HeaderComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
